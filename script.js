6e3628338dafb9376d3f5659bb00d393c1458504
chadhas282-afk <chadhas282@gmail.com>
2026-03-15 11:36:30 +0530

    fix: add data function for processing search results

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ volume = wrapper.querySelector(".word i"),
 infoText = wrapper.querySelector(".info-text"),
 synonyms = wrapper.querySelector(".synonyms .list"),
 removeIcon = wrapper.querySelector(".search span");
+
+function data(result, word){
+    
+}

e4a7803fcb4dd408368cee66e72d001626322e93
chadhas282-afk <chadhas282@gmail.com>
2026-03-15 11:36:43 +0530

    fix: update data function to handle search result errors

--- script.js
+++ script.js
@@ -6,5 +6,7 @@ synonyms = wrapper.querySelector(".synonyms .list"),
 removeIcon = wrapper.querySelector(".search span");
 
 function data(result, word){
-    
+    if(result.title){
+        infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+    }
 }
